0dfb4e8038d3b55c87a39a9ee32ace182d6d8e62
IamNaeto <egesionucharlesobimnaeto@gmail.com>
2023-05-16 22:02:41 +0100

    script adjust

--- script.js
+++ script.js
@@ -10,16 +10,16 @@ function bmiCalculation(){
     // convert height to meters
     bmi = weight.value / (height.value/100) ** 2  
     if(bmi < 18.5){
-        resInfo.textContent = `Hello, your BMI is ${bmi.toFixed(1)} and you are underweight, please try and add weight`
+        resInfo.textContent = `Hello, your BMI is ${bmi.toFixed(1)} and you are underweight, please try and add weight.`
         resInfo.style.color = "#004900"
     }else if(bmi <= 24.9){
-        resInfo.textContent = `Hello, your BMI is ${bmi.toFixed(1)} Congratulation! you have a healthy weight`
+        resInfo.textContent = `Hello, your BMI is ${bmi.toFixed(1)} Congratulations! you have a healthy weight.`
         resInfo.style.color = "#004900"
     }else if(bmi <=29.9){
-        resInfo.textContent = `Hello, your BMI is ${bmi.toFixed(1)} and you are overweight, please try exercising and eat healthy to loose weight`
+        resInfo.textContent = `Hello, your BMI is ${bmi.toFixed(1)} and you are overweight, please try exercising and eat healthy to loose weight.`
         resInfo.style.color = "#004900"
     }else if(bmi >=30.0){
-        resInfo.textContent = `Hello your BMI is ${bmi.toFixed(1)} Please visit a doctor for an advice because you are actually obese`
+        resInfo.textContent = `Hello your BMI is ${bmi.toFixed(1)} Please visit a doctor for an advice because you are actually obese.`
         resInfo.style.color = "#004900"
     }else{
         resInfo.textContent = "Invalid values!!!"
